e8589ba96699557ba4506d0f325eaf012cd3ea17
hbabuka <hristijan.babuka@gmail.com>
2023-01-14 14:27:14 +0100

    Setting up the API

---
+++ src/api.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+
+// Base URL
+const BASE_URL = "https://api.rawg.io/api/";
+
+// Getting the current date
+const getCurrentMonth = () => {
+  const month = new Date().getMonth() + 1;
+  if (month < 10) {
+    return `0${month}`;
+  } else return month;
+};
+
+const getCurrentDay = () => {
+  const day = new Date().getDate();
+  if (day < 10) {
+    return `0${day}`;
+  } else return day;
+};
+
+const currentYear = new Date().getFullYear();
+const currentMonth = getCurrentMonth();
+const currentDay = getCurrentDay();
+
+const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
+
+const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
+const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
+
+// Query parameters
+const POPULAR_GAMES = `games?key=${process.env.REACT_APP_API_KEY}&dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+
+// Popular Games URL
+const popularGamesURL = () => `${BASE_URL}${POPULAR_GAMES}`;
+
+console.log(popularGamesURL());
