ab8db93ec74f74a1e299fd36db81cc22d933d1b0
hbabuka <hristijan.babuka@gmail.com>
2023-02-24 15:10:01 +0100

    Styling on hover of screenshot images

--- src/components/GameScreenshots.js
+++ src/components/GameScreenshots.js
@@ -1,3 +1,4 @@
+import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import React from "react";
 import { useSelector } from "react-redux";
 import { GalleryImageModal } from "./GalleryImageModal";
@@ -13,13 +14,25 @@ export const GameScreenshots = () => {
         <h4 className="mb-6">Screenshots</h4>
         <div className="grid grid-cols-3 gap-4">
           {screenshots.results?.map((screenshot) => (
-            <GalleryImageModal src={screenshot.image} alt={screenshot.id}>
-              <img
-                src={screenshot.image}
-                key={screenshot.id}
-                alt={game.name}
-                className="rounded-[20px] h-52 object-cover"
-              />
+            <GalleryImageModal
+              src={screenshot.image}
+              alt={screenshot.id}
+              children
+            >
+              <div className="relative screenshot-media rounded-[20px] overflow-hidden hover:shadow-lg cursor-pointer">
+                <img
+                  src={screenshot.image}
+                  key={screenshot.id}
+                  alt={game.name}
+                  className="object-cover h-52"
+                />
+                <div className="hover-container opacity-0 z-10 bg-secondary-900 absolute inset-0 duration-300 flex items-center justify-center">
+                  <div className="flex items-center gap-1">
+                    <p className="text-white">Open</p>
+                    <ArrowRightIcon className="w-5 h-5 text-white" />
+                  </div>
+                </div>
+              </div>
             </GalleryImageModal>
           ))}
         </div>
