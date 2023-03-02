5548f89bec4248daebd3cf0684086bc05a545dc4
hbabuka <hristijan.babuka@gmail.com>
2023-03-02 13:20:59 +0100

    Refactor Spinner component

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,8 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { ReactComponent as SpinnerSvg } from "../assets/logo-spinner-secondary.svg";
-import { navHeight } from "../constants";
 
-export const Spinner = ({ mobile }) => {
+export const Spinner = () => {
   return (
     <div
       className={`spinner-container w-full h-[calc(100%-80px)] sm:h-[calc(100%-104px)] flex items-center justify-center`}
